ee7985a009a81edf90794a2d4215b4eb176139ac
Alex Ruaux <alex@molecularcodewebdesign.com>
2016-05-02 23:48:38 +0000

    ex4+5 - installed node mysql, retreived data from DB and displayed as html, sent form.html to page

--- index.js
+++ index.js
@@ -12,6 +12,7 @@ app.get('/hello', function (req, res) {
 
 
 // Exercise 2: A wild parameter has appeared!
+// -----------------------------------------------------------------------------
 // Create a web server that can listen to requests for /hello?name=firstName, and respond with some HTML that says <h1>Hello _name_!</h1>. For example, if a client requests /hello/John, the server should respond with <h1>Hello John!</h1>.
 
 function sayHelloTo(name) {
@@ -26,6 +27,7 @@ app.get('/hello/:name', function(request, response) {
 
 
 // Exercise 3: Operations
+// -----------------------------------------------------------------------------
 // Create a web server that can listen to requests for /calculator/:operation?num1=XX&num2=XX and respond with a JSON object that looks like the following. For example, /op/add?num1=31&num2=11:
 
 function mathFn(oper, num1, num2) {
@@ -65,7 +67,7 @@ app.get('/op/:operation/:num1/:num2', function(request, response) {
   
   if (result === "error" ) {
     response.status(404); 
-    response.send("<h2>error - please choose an operator from 'add, sub, mult, div'</h2>");
+    response.send("<h2>ERROR: please choose an operator from <i>'add, sub, mult, div'</i></h2>");
   }
   else {
     response.send(result);
@@ -79,6 +81,97 @@ app.get('/op/:operation/:num1/:num2', function(request, response) {
 //    solution	"3111"
 
 
+// Exercise 4: Retrieving data from our database
+// -----------------------------------------------------------------------------
+// Using something similar to your getHomepage function, or even directly the function itself, retrieve the latest 5 posts by createdAt date, including the username who created the content.
+
+// Once you have the query, create an endpoint in your Express server which will respond to GET requests to /posts. The Express server will use the MySQL query function to retrieve the array of contents. Then, you should build a string of HTML that you will send with the request.send function.
+
+// load the mysql library
+var mysql = require('mysql');
+
+// create a connection to our Cloud9 server
+var connection = mysql.createConnection({
+  host     : 'localhost',
+  user     : 'molecularcode',
+  password : '',
+  database : 'reddit',
+  debug    : false
+});
+
+function getPosts(callback) {
+  connection.query(`
+    SELECT p.id AS pId, p.title AS pTitle, p.url AS pURL, p.userId AS pUserId, p.createdAt AS pCreatedAt, p.updatedAt AS pUpdatedAt, u.username AS uUsername
+    FROM posts AS p
+    LEFT JOIN users AS u ON u.id = p.userId
+    ORDER BY p.createdAt DESC
+    LIMIT 5;
+    `, function(err, results) {
+    if (err) {
+      callback(err);
+    }
+    else {
+      var posts = results.map(function(results) {
+        var pObj = {
+          "id": results.pId,
+          "title": results.pTitle,
+          "url": results.pURL,
+          "userId": results.pUserId,
+          "createdAt": results.pCreatedAt,
+          "updatedAt": results.pUpdatedAt,
+          "username": results.uUsername
+        };
+        return pObj;
+      });
+      callback(null, posts);
+    }
+  });
+}
+
+function postToHTML(result) {
+  var htmlStart = '<div id="contents"> <h1>List of contents</h1> <ul class="contents-list">';
+  var htmlEnd = '</ul> </div>';
+  var postHTML = result.map(function(res){
+    return (
+      `<li class="content-item">
+        <h2 class="content-item__title"><a href="${res.url}">${res.title}</a></h2>
+        <p>Created by ${res.username} </p>
+      </li>`);
+  });
+  return (htmlStart + postHTML.join('') + htmlEnd);
+}
+
+app.get('/posts', function(req, res) {
+  getPosts(function(err, result) {
+    if (err) {
+      res.status(500).send('<h2>ERROR!</h2>');
+    }
+    else {
+      res.send(postToHTML(result));
+    }
+  });
+});
+
+
+// Exercise 5: Creating a "new content" form
+// -----------------------------------------------------------------------------
+// In this exercise, we're going to use Express to simply send an HTML file to our user containing a <form>
+
+app.get('/createContent', function(req, res) {
+  var options = {
+    root: __dirname + '/'
+  };
+
+  //var fileName = req.params.name;
+  res.sendFile('form.html', options, function(err) {
+    if (err) {
+      res.status(500).send('<h2>ERROR!</h2>' + err);
+    }
+    else {
+      console.log('Sent: form.html');
+    }
+  });
+});
 
 
 /* YOU DON'T HAVE TO CHANGE ANYTHING BELOW THIS LINE :) */

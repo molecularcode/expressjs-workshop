c1ec48da5227a9d58d7559a3c8096a5201a3d859
Alex Ruaux <alex@molecularcodewebdesign.com>
2016-05-02 19:38:40 +0000

    ex3 - pass multiple parameters from the 'url' to a GET function

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ var app = express();
 
 
 // Exercise 1: Getting started!
+// -----------------------------------------------------------------------------
 // Create a web server that can listen to requests for /hello, and respond with some HTML that says <h1>Hello World!</h1>
 
 app.get('/hello', function (req, res) {
@@ -22,6 +23,53 @@ app.get('/hello/:name', function(request, response) {
     var result = sayHelloTo(name);
     response.send(result);
 });
+
+
+// Exercise 3: Operations
+// Create a web server that can listen to requests for /calculator/:operation?num1=XX&num2=XX and respond with a JSON object that looks like the following. For example, /op/add?num1=31&num2=11:
+
+function mathFn(oper, num1, num2) {
+  var result = {
+    "operator": oper,
+    "firstOperand": num1,
+    "secondOperand": num2,
+  };
+  if(oper === "add") {
+    result.solution = num1+num2;
+    return result;
+  }
+  else if(oper === "sub") {
+    result.solution = num1-num2;
+    return result;
+  }
+  else if(oper === "mult") {
+    result.solution = num1*num2;
+    return result;
+  }
+  else if(oper === "div") {
+    result.solution = num1/num2;
+    return result;
+  }
+  else {
+    return "error - please choose an operator from 'add, sub, mult, div";
+  }
+}
+
+app.get('/op/:operation/:num1/:num2', function(request, response) {
+  var oper = request.params.operation;
+  var num1 = request.params.num1;
+  var num2 = request.params.num2;
+  
+  var result = mathFn(oper, num1, num2);
+  response.send(result);
+});
+
+// => /op/add/31/11
+// => operator	"add"
+//    firstOperand	"31"
+//    secondOperand	"11"
+//    solution	"3111"
+
 
 
 

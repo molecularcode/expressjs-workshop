4f69e1727cbcbba7b15953ea7d28fcd349b2e4b3
Alex Ruaux <alex@molecularcodewebdesign.com>
2016-05-02 18:47:24 +0000

    ex2 - use req.params to take a parameter and use it in a query string

--- index.js
+++ index.js
@@ -1,11 +1,27 @@
 var express = require('express');
 var app = express();
 
+
+// Exercise 1: Getting started!
+// Create a web server that can listen to requests for /hello, and respond with some HTML that says <h1>Hello World!</h1>
+
 app.get('/hello', function (req, res) {
   res.send('<h1>Hello World!</h1>');
 });
 
 
+// Exercise 2: A wild parameter has appeared!
+// Create a web server that can listen to requests for /hello?name=firstName, and respond with some HTML that says <h1>Hello _name_!</h1>. For example, if a client requests /hello/John, the server should respond with <h1>Hello John!</h1>.
+
+function sayHelloTo(name) {
+    return "<h1>hello " + name + "!</h1>";
+}
+
+app.get('/hello/:name', function(request, response) {
+    var name = request.params.name;
+    var result = sayHelloTo(name);
+    response.send(result);
+});
 
 
 
